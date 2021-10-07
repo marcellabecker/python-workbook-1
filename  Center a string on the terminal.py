1ac2d8fd8838e4098af8a042d37b79e777188a66
marcellabecker <marcellabecker1@gmail.com>
2021-10-07 00:11:26 -0300

    create center a string on the terminal.py

---
+++  Center a string on the terminal.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python3
+import os
+stringparam = input()
+columns, lines = os.get_terminal_size()
+size = len(stringparam)
+columns = (columns//2) + (size//2)
+ste = stringparam.rjust(int(columns))
+print(ste)
